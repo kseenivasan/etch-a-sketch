ededa55d15626e8cf22bbbe1d8f633dcc7eeec36
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-20 13:23:06 +0530

    Add functionality to button to resize grid

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -9,23 +9,61 @@ buttonContainer.appendChild(button);
 buttonContainer.style.justifyContent = 'center';
 buttonContainer.style.alignItems = 'center';
 mainDiv.appendChild(buttonContainer);
-for (let i = 0; i < 16; i++) {
-    let flexContainer = document.createElement('div');
-    flexContainer.style.display = 'flex';
-    for (let j = 0; j < 16; j++) {
-        let div = document.createElement('div');
-        div.style.width = '60px';
-        div.style.height = '60px';
-        div.style.flex = '1 1 auto';
-        div.classList.add("grid");
-        flexContainer.appendChild(div);
+
+
+const gridWidth = 760;
+function createGrid(gridWidth, numSquares) {
+    for (let i = 0; i < numSquares; i++) {
+        let flexContainer = document.createElement('div');
+        flexContainer.classList.add("flex-container");
+        flexContainer.style.display = 'flex';
+        for (let j = 0; j < numSquares; j++) {
+            let div = document.createElement('div');
+            div.style.width = gridWidth/numSquares + 'px';
+            div.style.height = gridWidth/numSquares + 'px';
+            div.style.flex = '0 1 auto';
+            div.classList.add("grid");
+            flexContainer.appendChild(div);
+        }
+        mainDiv.appendChild(flexContainer);
     }
-    mainDiv.appendChild(flexContainer);
+    
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach( (grid) => grid.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'yellow';
+        console.log("Hovered");
+    }));
 }
 
-const grids = document.querySelectorAll(".grid");
+function removeGrid() {
+    let deletionList = Array.from(document.querySelectorAll('.flex-container'));
+    while (deletionList.length > 0) {
+        let toRemove = deletionList.pop();
+        toRemove.parentNode.removeChild(toRemove);
+    }
 
-grids.forEach( (grid) => grid.addEventListener('mouseover', (event) => {
-    event.target.style.backgroundColor = 'yellow';
-    console.log("Hovered");
-}));
+}
+createGrid(760, 16);
+
+
+
+
+button.addEventListener('click', () => {
+    let input = prompt("Please enter number of squares on each side (max 100)");
+    if (Number.isNaN(input)  || input > 100) {
+        let valid = false;
+        while (!valid) {
+            input = prompt("Input was invalid, please try again");
+            if (Number.isNaN(input) || input > 100) {
+                valid = false;
+            }
+            else {
+                valid = true;
+            }
+        }
+    }
+    else {
+        removeGrid();
+        createGrid(gridWidth,Number.parseInt(input));
+    }
+})
