189571fd14a5a68c862fb51d1a90e91de7a9f72b
kseenivasan <karthikseenivasan94@gmail.com>
2023-01-20 12:22:37 +0530

    Fix grid width height and add mouseover listener

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -5,9 +5,18 @@ for (let i = 0; i < 16; i++) {
     flexContainer.style.display = 'flex';
     for (let j = 0; j < 16; j++) {
         let div = document.createElement('div');
-        div.style.color = 'blue';
-        div.textContent = "Testing 123";
+        div.style.width = '60px';
+        div.style.height = '60px';
+        div.style.flex = '1 1 auto';
+        div.classList.add("grid");
         flexContainer.appendChild(div);
     }
     mainDiv.appendChild(flexContainer);
 }
+
+const grids = document.querySelectorAll(".grid");
+
+grids.forEach( (grid) => grid.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'yellow';
+    console.log("Hovered");
+}));
